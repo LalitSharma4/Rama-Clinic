c70e1ab8d9126775b38c5c28ba9e3b7cc3864b33
LalitSharma4 <iamlalitsharma4@gmail.com>
2026-07-08 09:39:25 +0530

    correct url

--- Auth_API/urls.py
+++ Auth_API/urls.py
@@ -3,10 +3,10 @@ from .views import *
 
 urlpatterns = [
 
-    path('register', UserRegisterAPI.as_view(), name='user-register'),
-    path("update/<int:user_id>", UpdateProfileAPI.as_view()),
-    path("check-mobile", CheckMobileAPI.as_view()),
-    path("users", GetUserAPI.as_view()),
+    path('register/', UserRegisterAPI.as_view(), name='user-register'),
+    path("update/<int:user_id>/", UpdateProfileAPI.as_view()),
+    path("check-mobile/", CheckMobileAPI.as_view()),
+    path("users/", GetUserAPI.as_view()),
    
     
 ]
